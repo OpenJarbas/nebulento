db994d6d87b99b574ef418994119e82cf7499bc9
JarbasAi <jarbasai@mailfence.com>
2023-09-21 14:24:46 +0100

    feat/pipeline_plugin

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+PLUGIN_ENTRY_POINT = 'ovos-intent-plugin-nebulento=nebulento.opm:NebulentoPipelinePlugin'
+
+
 setup(
     name='nebulento',
     version='0.1.0',
@@ -9,5 +12,6 @@ setup(
     author='jarbasai',
     author_email='jarbasai@mailfence.com',
     install_requires=["quebra_frases", "rapidfuzz"],
-    description='dead simple fuzzy matching intent parser'
+    description='dead simple fuzzy matching intent parser',
+    entry_points={'ovos.pipeline': PLUGIN_ENTRY_POINT}
 )
